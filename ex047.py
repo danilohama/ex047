ec54657101a02f0464c7b5cc0016ea3b121ec0f6
Danilo ohama <91641703+danilohama@users.noreply.github.com>
2023-09-07 17:40:01 -0300

    Add files via upload

---
+++ ex047.py
@@ -0,0 +1,6 @@
+"""Crie um programa que mostre na tela todos os números pares que estão no intervalo de 1 a 50
+"""
+for numero in range(2, 51, 2):
+    # if numero % 2 == 0:
+    print(numero, end=' ')
+print('ACABOU!')
